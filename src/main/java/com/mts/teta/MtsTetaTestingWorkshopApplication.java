5f6013b7995f4a26f41ef280c07a3f2b6a5ef5d3
Семен Киреков <sakireko@mts.ru>
2022-12-11 11:19:32 +0300

    init

---
+++ src/main/java/com/mts/teta/MtsTetaTestingWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mts.teta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsTetaTestingWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsTetaTestingWorkshopApplication.class, args);
+	}
+
+}
